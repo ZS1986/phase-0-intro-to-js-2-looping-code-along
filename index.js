2126be2a7e889de39e01135404995d4fc3fb2ff7
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-26 16:42:38 -0600

    commit tree 00817d19411b28f44019f12d3efb8d364cd544ce

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(array,eventName){
+    let messageArray = [];
+    for (let i = 0; i < array.length; i++){
+        let name = array[i];
+        let message = 
+        messageArray.push(message);
+    }
+
+return messageArray;
+}
+
+  
